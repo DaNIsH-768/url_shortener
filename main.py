9425bcf422e5e09bf4ec0feb0783079b4d2c3503
Danish Raina <danishraina768@gmail.com>
2026-06-01 22:39:19 +0100

    Added signin and signup page with styling

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse(request, name='home.html')
 
+@app.get("/signin")
+async def signin(request: Request):
+    return templates.TemplateResponse(request, name='signin.html')
 
+@app.get("/signup")
+async def signup(request: Request):
+    return templates.TemplateResponse(request, name='signup.html')
