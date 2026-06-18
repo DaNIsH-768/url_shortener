06cd4efa50cd31f675247ee45ce02bd5bd32de4b
Danish Raina <danishraina768@gmail.com>
2026-06-18 01:16:19 +0100

    Add login endpoint with JWT token generation

--- routers/auth.py
+++ routers/auth.py
@@ -3,7 +3,13 @@ from sqlmodel import Session, select
 from passlib.hash import bcrypt
 from database import engine
 from models import User
+import jwt
+from datetime import datetime, timedelta
+import os
 from pydantic import BaseModel
+from dotenv import load_dotenv
+
+load_dotenv()
 
 router = APIRouter()
 
@@ -11,6 +17,10 @@ class RegisterParams(BaseModel):
     email: str = ""
     password: str = ""
     confirm_password: str = ""
+
+class LoginParams(BaseModel):
+    email: str = ""
+    password: str = ""
 
 def select_email(email:str):
     with Session(engine) as session:
@@ -44,6 +54,32 @@ async def register(register_query: RegisterParams):
     except Exception as e:
         print(e)
         return {"message": "Error while registering"}
+
+
+@router.post("/auth/login", status_code=200)
+async def login(login_query: LoginParams):
+    email, password = login_query.email, login_query.password
+
+    user = select_email(email)
+
+    if not user:
+        raise HTTPException(status_code=400, detail="User not registered.")
+    
+    if not bcrypt.verify(password, user.password):
+        raise HTTPException(status_code=401, detail="Password incorrect.")
+
+    exp = datetime.utcnow() + timedelta(minutes=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")))
+
+    payload = {
+        "user_id": user.id,
+        "exp": exp
+    }
+
+    jwt_access = jwt.encode(payload, os.getenv("SECRET_KEY"), algorithm=os.getenv("ALGORITHM"))
+
+    return jwt_access
+
+
 
     
 
